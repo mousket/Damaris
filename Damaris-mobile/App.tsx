6c22d09d349058e4a206da3f0698893743848614
Andyrub18 <andersonruban1281@gmail.com>
2024-06-04 23:30:23 -0400

    Added router, new packages and recording configurations

--- Damaris-mobile/App.tsx
+++ Damaris-mobile/App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AudioRecord from "./src/screens/AudioRecord";
+
+export type RootStackNav = {
+	AudioRecord: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackNav>();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="AudioRecord"
+					component={AudioRecord}
+					options={{ title: "Audio record", headerTitleAlign: "center" }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
